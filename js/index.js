38be2c35578a5a1acf9bf637b8a19935285c0a68
Regina <reina5425@gmail.com>
2022-06-30 21:49:36 +0300

    feat: add js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+function calcPlus () {
+	let a = +prompt('Введите первое число:', '');
+	let b =  +prompt('Введите второе число:', '');
 
+	return alert(`Ответ: ${a + b}`);
+}
+
+function calcMinus () {
+	let a = +prompt('Введите первое число:', '');
+	let b =  +prompt('Введите второе число:', '');
+
+	return alert(`Ответ: ${a - b}`);
+}
+
+function calcMultiple () {
+	let a = +prompt('Введите первое число:', '');
+	let b =  +prompt('Введите второе число:', '');
+
+	return alert(`Ответ: ${a * b}`);
+}
+
+function calcDivision () {
+	let a = +prompt('Введите первое число:', '');
+	let b =  +prompt('Введите второе число:', '');
+
+	return alert(`Ответ: ${a / b}`);
+}
