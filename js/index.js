d82eeebaba9f24c50493bb4d2d019245216372c0
Regina <reina5425@gmail.com>
2022-07-08 20:11:36 +0300

    feat: modify js functions

--- js/index.js
+++ js/index.js
@@ -1,27 +1,64 @@
-function calcPlus () {
-	let a = +prompt('Введите первое число:', '');
-	let b =  +prompt('Введите второе число:', '');
+// function calcPlus () {
+// 	let a = +prompt('Введите первое число:', '');
+// 	let b =  +prompt('Введите второе число:', '');
 
-	return alert(`Ответ: ${a + b}`);
+// 	return alert(`Ответ: ${a + b}`);
+// }
+
+// function calcMinus () {
+// 	let a = +prompt('Введите первое число:', '');
+// 	let b =  +prompt('Введите второе число:', '');
+
+// 	return alert(`Ответ: ${a - b}`);
+// }
+
+// function calcMultiple () {
+// 	let a = +prompt('Введите первое число:', '');
+// 	let b =  +prompt('Введите второе число:', '');
+
+// 	return alert(`Ответ: ${a * b}`);
+// }
+
+// function calcDivision () {
+// 	let a = +prompt('Введите первое число:', '');
+// 	let b =  +prompt('Введите второе число:', '');
+
+// 	return alert(`Ответ: ${a / b}`);
+// }
+
+const num1 = document.querySelector('.calc__num--1');
+const num2 = document.querySelector('.calc__num--2');
+
+let firstNum = function () {
+	return Number(num1.value);
+};
+
+let secondNum = function () {
+	return Number(num2.value);
+};
+
+let result;
+
+function pasteResult() {
+	const calcResult = document.querySelector('.calc__result');
+	calcResult.textContent = `Ответ: ${result}`;
+
+	const totalOutput = document.querySelector('.calc__result');
+	totalOutput.style.backgroundColor = '#fff';
 }
 
-function calcMinus () {
-	let a = +prompt('Введите первое число:', '');
-	let b =  +prompt('Введите второе число:', '');
-
-	return alert(`Ответ: ${a - b}`);
+function calcPlus() {
+	result = firstNum() + secondNum();
 }
 
-function calcMultiple () {
-	let a = +prompt('Введите первое число:', '');
-	let b =  +prompt('Введите второе число:', '');
-
-	return alert(`Ответ: ${a * b}`);
+function calcMinus () {	
+	result = firstNum() - secondNum();
 }
 
-function calcDivision () {
-	let a = +prompt('Введите первое число:', '');
-	let b =  +prompt('Введите второе число:', '');
+function calcMultiple () {	
+	result = firstNum() * secondNum();
+}
 
-	return alert(`Ответ: ${a / b}`);
+function calcDivision () {	
+	result = firstNum() / secondNum();
 }
